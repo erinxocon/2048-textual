411a53f1f9703ed8f7d3e2ae922bec99a255ea73
Erin O'Connell <erinocon5@gmail.com>
2022-03-19 23:27:05 -0600

    Refactor app logic

--- logic.py
+++ logic.py
@@ -1,17 +1,40 @@
-import numpy as np
 from typing import Optional
+
+import numpy as np
+
+from .enums import GameStates, MoveType
 
 
 class TwentyFortyEight:
-    def __init__(self, initial_state: Optional[np.ndarray], size: int = 4) -> None:
-        self.game: np.ndarray = initial_state if initial_state is not None else np.zeros((size, size), dtype=int)
-
-    def __repr__(self) -> str:
-        return f'{self.game}\n'
+    def __init__(self, initial_state: Optional[np.ndarray] = None, size: int = 4) -> None:
+        self._num_turns = 0
+        if initial_state is not None:
+            self.game = initial_state
+        else:
+            self.game = np.zeros((size, size), dtype=int)
+            self.add_new_tile()
+        self._game_state = GameStates.can_play
 
     @property
     def size(self) -> int:
         return self.game.shape[0]
+
+    @property
+    def num_turns(self) -> int:
+        return self._num_turns
+
+    @property
+    def game_state(self) -> GameStates:
+        return self._game_state
+
+    @game_state.setter
+    def game_state(self, value: GameStates) -> None:
+        self._game_state = value
+
+    def add_new_tile(self) -> None:
+        remaining_zeros = np.argwhere(self.game == 0)
+        random_idx_row, random_idx_col = remaining_zeros[np.random.randint(remaining_zeros.shape[0], size=1)][0]
+        self.game[random_idx_row, random_idx_col] = 2
 
     def merge_cells(self):
         for i in range(self.size):
@@ -36,29 +59,50 @@ class TwentyFortyEight:
     def move_left(self) -> None:
         self.compress_cells().merge_cells().compress_cells()
 
-    def move_right(self) -> None:
-        self.game = np.fliplr(self.game)
-        self.move_left()
-        self.game = np.fliplr(self.game)
+    def make_move(self, move_type: MoveType) -> None:
+        match move_type:
+            case MoveType.LEFT:
+                self.move_left()
+            case MoveType.RIGHT:
+                self.game = np.fliplr(self.game)
+                self.move_left()
+                self.game = np.fliplr(self.game)
+            case MoveType.UP:
+                self.game = np.transpose(self.game)
+                self.move_left()
+                self.game = np.transpose(self.game)
+            case MoveType.DOWN:
+                self.game = np.transpose(self.game)
+                self.game = np.fliplr(self.game)
+                self.move_left()
+                self.game = np.fliplr(self.game)
+                self.game = np.transpose(self.game)
 
-    def move_up(self) -> None:
-        self.game = np.transpose(self.game)
-        self.move_left()
-        self.game = np.transpose(self.game)
+        # increment move counter now that a move has been made
+        self._num_turns += 1
 
-    def move_down(self) -> None:
-        self.game = np.transpose(self.game)
-        self.game = np.fliplr(self.game)
-        self.move_left()
-        self.game = np.transpose(self.game)
-        self.game = np.fliplr(self.game)
+        # check if we can still play and add a tile
+        if 0 in self.game:
+            self.add_new_tile()
 
+        # check for any win states
+        elif 2048 in self.game:
+            self.game_state = GameStates.won
 
-if __name__ == '__main__':
-    a = np.array([[0, 2, 4, 4], [2, 2, 4, 2], [0, 2, 4, 0], [2, 2, 4, 4]])
+        # elif self.can_move_be_made():
+        #     pass
 
-    b = TwentyFortyEight(initial_state=a)
-    print(b.game)
-    b.move_up()
-    print(b.game)
-    print('test')
+        else:
+            self.game_state = GameStates.lost
+            raise NotImplementedError()
+
+    def can_move_be_made(self) -> bool:
+        return False
+
+    def __str__(self) -> str:
+        return f"""
+Game Board:
+ {np.array_str(self.game)[1:-1]}
+Game State: {self.game_state.value}
+Number of Turns: {self.num_turns}
+"""
